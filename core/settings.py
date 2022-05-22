be32d058a5805f2c18bf93016bbdff0150d1cf24
Bart Strzyga <87241975+bartstr@users.noreply.github.com>
2022-05-22 12:06:43 +0200

    Created Post and PostLoading modules in react part of app.

--- core/settings.py
+++ core/settings.py
@@ -44,11 +44,13 @@ INSTALLED_APPS = [
     'blog',
     'blog_api',
     'rest_framework',
+    'corsheaders',
 ]
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
+    'corsheaders.middleware.CorsMiddleware',
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
     'django.contrib.auth.middleware.AuthenticationMiddleware',
@@ -133,3 +135,6 @@ REST_FRAMEWORK = {
         'rest_framework.permissions.AllowAny',
     ]
 }
+CORS_ALLOWED_ORIGINS = [
+    "http://localhost:3000"
+]
