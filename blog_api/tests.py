1a0c26daa85e4a53f0ba4e3040ed4093f37b40fa
Bart Strzyga <87241975+bartstr@users.noreply.github.com>
2022-05-23 17:44:15 +0200

    Created custom permissions to PostDetail view in blog_api and created/adjusted tests to correspond to abovementioned change.

--- blog_api/tests.py
+++ blog_api/tests.py
@@ -1,7 +1,7 @@
 from django.urls import reverse
 from rest_framework import status
-from rest_framework.test import APITestCase
-from blog.models import Category
+from rest_framework.test import APIClient, APITestCase
+from blog.models import Category, Post
 from django.contrib.auth.models import User
 
 
@@ -14,11 +14,28 @@ class PostTests(APITestCase):
         self.assertEqual(response.status_code, status.HTTP_200_OK)
 
     def create_post(self):
+        client = APIClient()
         self.test_category = Category.objects.create(name='django')
-        self.test_user = User.objects.create_user(
+        self.test_user = User.objects.create_superuser(
             username='test_user', password='password123')
+        client.login(username=self.test_user.username, password='password123')
         data = {"title": "new", "author": 1, "excerpt": "new", "content": "new"}
         url = reverse('blog_api:listcreate')
         response = self.client.post(url, data, format='json')
         self.assertEqual(response.status_code, status.HTTP_201_CREATED)
 
+    def test_post_update(self):
+        client = APIClient()
+        self.test_category = Category.objects.create(name='django')
+        self.test_user = User.objects.create_user(
+            username='test_user', password='password123')
+        self.test_user2 = User.objects.create_user(
+            username='test_user2', password='password1234')
+        test_post = Post.objects.create(category_id=1, title='Post title', excerpt='old', content='New', author_id=1,
+                                        slug='post-title', status='published')
+        url = reverse(('blog_api:detailcreate'), kwargs={'pk': 1})
+        client.login(username=self.test_user.username, password='password123')
+        response = client.put(
+            url, {"title": "new", "author": 1, "excerpt": "new", "content": "changed", "status": "published"},
+            format='json')
+        self.assertEqual(response.status_code, status.HTTP_200_OK)
--- blog_api/views.py
+++ blog_api/views.py
@@ -1,13 +1,26 @@
 from rest_framework import generics
 from blog.models import Post
 from .serializers import PostSerializer
+from rest_framework.permissions import IsAdminUser, DjangoModelPermissions, BasePermission, SAFE_METHODS, \
+    IsAuthenticatedOrReadOnly
+
+
+class PostUserWritePermission(BasePermission):
+    message = 'Editing posts is restricted to the author only.'
+
+    def has_object_permission(self, request, view, obj):
+        if request.method in SAFE_METHODS:
+            return True
+        return obj.author == request.user
 
 
 class PostList(generics.ListCreateAPIView):
     queryset = Post.postobjects.all()
     serializer_class = PostSerializer
+    permission_classes = [IsAuthenticatedOrReadOnly]
 
 
-class PostDetail(generics.RetrieveDestroyAPIView):
+class PostDetail(generics.RetrieveUpdateDestroyAPIView, PostUserWritePermission):
     queryset = Post.objects.all()
     serializer_class = PostSerializer
+    permission_classes = [PostUserWritePermission]
